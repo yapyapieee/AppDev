3de5b03264f977029f64bfd125536f9658a62a02
Pia <piadtantay@su.edu.ph>
2024-10-05 20:03:47 +0800

    added code for authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'your_secret_key'; // Use the same key as in userController
+const SECRET_KEY = 'your_secret_key';
 
+//authetnicate user
 exports.authenticate = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) return res.status(401).send('Access denied.');
+    if (!token) {
+        return res.status(401).send('Access denied.');
+    }
 
+    //token verification
     jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).send('Invalid token.');
+        if (err) {
+            return res.status(403).send('Invalid token.');
+        }
         req.user = user;
         next();
     });
